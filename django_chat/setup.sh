cb9f2e25b34df57b5014613c58dd24b325a06b79
matejatasic <70795666+matejatasic@users.noreply.github.com>
2024-10-12 14:00:40 +0200

    fix installing redis not working

--- django_chat/setup.sh
+++ django_chat/setup.sh
@@ -18,8 +18,8 @@ curl -fsSL https://packages.redis.io/gpg | sudo gpg --dearmor -o /usr/share/keyr
 sudo chmod 644 /usr/share/keyrings/redis-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/redis-archive-keyring.gpg] https://packages.redis.io/deb $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/redis.list
 sudo apt-get update
-sudo apt-get install redis
-sudo systemctl start redis
+sudo apt-get -y install redis
+sudo systemctl start redis-server
 
 # pip installs
 pip3 install django
